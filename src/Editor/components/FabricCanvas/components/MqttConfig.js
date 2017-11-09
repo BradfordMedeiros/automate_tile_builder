f8ae682b366dc0402876d2d831873c96067d7807
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-11-08 19:55:23 -0800

    some work on mqtt config component

--- src/Editor/components/FabricCanvas/components/MqttConfig.js
+++ src/Editor/components/FabricCanvas/components/MqttConfig.js
@@ -13,11 +13,42 @@ class MqttConfig extends Component {
   };
   handleChange = (event, index, value) => this.setState({value});
 
+  hasChanged = () => {
+    const { mqttTopic, condition, mqttValue } = this.props;
+    if (this.state.mqttTopic !== null && this.state.mqttTopic !== mqttTopic) {
+      return true;
+    }
+    if (this.state.condition !== null && this.state.condition !== condition){
+      return true;
+    }
+    if (this.state.mqttValue !== null && this.state.mqttValue !== mqttValue){
+      return true;
+    }
+    return false;
+  };
+  handleRevert = () => {
+    const { onRevert } = this.props;
+    this.setState({
+      mqttTopic: null,
+      condition: null,
+      mqttValue: null,
+    });
+    onRevert();
+  };
+  handleApply = () => {
+
+  };
   render() {
     return (
       <div style={{ padding: 18, border: '1px solid black', display: 'flex',  flexDirection: 'column' }}>
         <div>Mqtt Options</div>
-        <TextField floatingLabelText="Mqtt Topic" />
+        <TextField
+          onChange={(_, mqttTopic) => {
+            console.log('on  change: ', mqttTopic);
+            this.setState({ mqttTopic });
+          }}
+          floatingLabelText="Mqtt Topic"
+        />
         <DropDownMenu value={this.state.value} onChange={this.handleChange}>
           <MenuItem value={'=='} primaryText={'=='} />
           <MenuItem value={'!='} primaryText={'!='}/>
@@ -28,8 +59,8 @@ class MqttConfig extends Component {
         </DropDownMenu>
         <TextField floatingLabelText="Mqtt Value" />
         <div>
-          <FlatButton label="Revert" />
-          <FlatButton label="Apply" />
+          <FlatButton onClick={this.handleRevert} disabled={!this.hasChanged()} label="Revert" />
+          <FlatButton onClick={this.handleApply} label="Apply" />
         </div>
       </div>
     );
@@ -37,10 +68,11 @@ class MqttConfig extends Component {
 };
 
 MqttConfig.propTypes = {
-  disabled: PropTypes.string,
   mqttTopic: PropTypes.string,
   condition: PropTypes.string,
   mqttValue: PropTypes.string,
+  onRevert: PropTypes.func,
+  onApply: PropTypes.func,
 };
 
 export default MqttConfig;
